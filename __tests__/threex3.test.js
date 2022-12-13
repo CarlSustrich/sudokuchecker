30b87a3e747b0d83fbe5975ccc8754ac54142031
Carl Sustrich <carl.sustrich@gmail.com>
2022-12-12 19:50:25 -0800

    pass 3x3 creates object test

--- __tests__/threex3.test.js
+++ __tests__/threex3.test.js
@@ -2,16 +2,16 @@ import ThreeBy3 from './../src/sudoku.js';
 
 describe ("ThreeBy3", () => {
   test("should correctly create an threeby3 object with 9 box values", () =>{
-    const threeby3 = new ThreeBy3(1,2,3,4,5,6,7,8,9);
-    expect(triangle.side1).toEqual(1);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side1).toEqual(3);
-    expect(triangle.side1).toEqual(4);
-    expect(triangle.side1).toEqual(5);
-    expect(triangle.side1).toEqual(6);
-    expect(triangle.side1).toEqual(7);
-    expect(triangle.side1).toEqual(8);
-    expect(triangle.side1).toEqual(9);
+    const threeby3 = new ThreeBy3(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    expect(threeby3.box1).toEqual(1);
+    expect(threeby3.box2).toEqual(2);
+    expect(threeby3.box3).toEqual(3);
+    expect(threeby3.box4).toEqual(4);
+    expect(threeby3.box5).toEqual(5);
+    expect(threeby3.box6).toEqual(6);
+    expect(threeby3.box7).toEqual(7);
+    expect(threeby3.box8).toEqual(8);
+    expect(threeby3.box9).toEqual(9);
   })
 })
 
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,13 +1,13 @@
 
 
-export default function ThreeBy3 (box1,  box2, box3, box4, box5, box6, box7, box8, box9) {
-  // this.box1 = box1;
-  // this.box2 = box2;
-  // this.box3 = box3;
-  // this.box4 = box4;
-  // this.box5 = box5;
-  // this.box6 = box6;
-  // this.box7 = box7;
-  // this.box8 = box8;
-  // this.box9 = box9;
+export default function ThreeBy3 (num1, num2, num3, num4, num5, num6, num7, num8, num9) {
+  this.box1 = num1;
+  this.box2 = num2;
+  this.box3 = num3;
+  this.box4 = num4;
+  this.box5 = num5;
+  this.box6 = num6;
+  this.box7 = num7;
+  this.box8 = num8;
+  this.box9 = num9;
 }
