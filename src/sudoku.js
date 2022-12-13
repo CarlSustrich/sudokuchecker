f6c4a8ba04f6324fe2245ae27b2763c8df75e70e
Carl Sustrich <carl.sustrich@gmail.com>
2022-12-12 21:19:39 -0800

    update createArray to clear values first, add findDuplicate function

--- src/sudoku.js
+++ src/sudoku.js
@@ -10,4 +10,24 @@ export default function ThreeBy3 (num1, num2, num3, num4, num5, num6, num7, num8
   this.box7 = num7;
   this.box8 = num8;
   this.box9 = num9;
+  this.arrayOfValues = [];
 }
+
+ThreeBy3.prototype.createArray = function () {
+  this.arrayOfValues.splice(0, 8);
+  let arr = Object.values(this);
+  arr.pop();
+  this.arrayOfValues.push(...arr);
+};
+
+ThreeBy3.prototype.findDuplicate = function () {
+  let valuesAlreadySeen = []
+  for (let i = 0; i < this.arrayOfValues.length; i++) {
+    let value = this.arrayOfValues[i]
+    if (valuesAlreadySeen.indexOf(value) !== -1) {
+      return true
+    }
+    valuesAlreadySeen.push(value)
+  }
+  return false
+};
